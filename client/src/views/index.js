6c32a261b0f2ab5ce4e951cea555e4e866cd0638
Ailín <ailingalanterosso@gmail.com>
2023-03-16 18:53:22 -0300

    NotFound page added

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -2,5 +2,6 @@ import Home from "./Home/Home";
 import Landing from "./Landing/Landing";
 import Detail from "./Detail/Detail";
 import Form from "./Form/Form";
+import NotFound from "./NotFound/NotFound"
 
-export { Home, Landing, Detail, Form }
+export { Home, Landing, Detail, Form, NotFound }
