4a1f03e90184510ca440d04ffe3b1ed5d8ad0834
Ailín <ailingalanterosso@gmail.com>
2023-03-19 16:29:29 -0300

    fixed logo

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom";
 import style from "./NavBar.module.css";
 import SearchBar from "../SearchBar/SearchBar";
+import logo from "../../assets/pokemon-text.png"
 
 const NavBar = () => {
     return(
         <div className={style.mainContainer}>
             <nav className={style.navbar}>
-                <div className={style.logo}><NavLink to="/home"><img src="https://www.freepnglogos.com/uploads/black-pokemon-logo-transparent-27.png" alt="logo"></img></NavLink></div>
+                <div className={style.logo}><NavLink to="/home"><img src={logo} alt="logo"></img></NavLink></div>
                 <div className={style.navLinks}>
                     <NavLink to="/create" className={style.link}>Create Pokemon</NavLink>
                     <NavLink to="/" className={style.link}>Logout</NavLink>
