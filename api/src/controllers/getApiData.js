02c7697a300d42a7aedae3fe4c7f1b5c5f3b6c0d
Ailín <ailingalanterosso@gmail.com>
2023-04-26 15:42:00 -0300

    poquitos

--- api/src/controllers/getApiData.js
+++ api/src/controllers/getApiData.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const getPokemonById = require("../controllers/getPokemonById");
 
 const getApiData = async (pokemons = []) =>{
-    const pokemonPromises = Array.from({length: 100}, (_, i) => getPokemonById(i + 1, "api"));
+    const pokemonPromises = Array.from({length: 200}, (_, i) => getPokemonById(i + 1, "api"));
     const resolvedPromises = await Promise.all(pokemonPromises);
     pokemons.push(...resolvedPromises);
     return pokemons;
