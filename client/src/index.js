336c7500b20dabde0b0222b9d46f4e90b17b0566
Ailín <ailingalanterosso@gmail.com>
2023-04-26 15:25:51 -0300

    route fixed

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import store from "./redux/store";
 import axios from "axios";
 
 // axios.defaults.baseURL= "http://localhost:3001"
-axios.defaults.baseURL= "https://pokeproyect.onrender.com/"
+axios.defaults.baseURL= "https://pokeproyect.onrender.com"
 
 ReactDOM.render(
   <Provider store={store}>
